78402b33a7cb13173cf9e8a582314847b95b6dec
dalipkumar4 <dalipkumar3559@gmail.com>
2026-01-15 13:43:40 +0530

    Added object.cpp for OOPS

---
+++ 3rd Sem/OOPS/object.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car { 
+public:    
+    string brand;
+    int year;
+
+    void displayDetails() { 
+        cout << "Brand: " << brand << ", Year: " << year << endl;
+    }
+};
+
+int main() {
+    Car myCar; // Create an object named 'myCar' of the 'Car' class
+
+    myCar.brand = "Toyota";
+    myCar.year = 2024;
+
+    myCar.displayDetails();
+
+    Car anotherCar;
+    anotherCar.brand = "Ford";
+    anotherCar.year = 2020;
+    anotherCar.displayDetails();
+
+    return 0;
+}
